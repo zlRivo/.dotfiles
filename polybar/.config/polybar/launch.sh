6c1930bea3715072ff52aa9519991074a4535e9a
Kiliany LeCossec <kiliany.lcssc@eduge.ch>
2023-06-17 01:15:42 +0200

    External monitor config bspwm and others

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,5 +1,7 @@
 # Killall polybars
 killall -q polybar
 
-polybar -q top --config=~/.config/polybar/config.ini > /dev/null & disown
-polybar -q bottom --config=~/.config/polybar/config.ini > /dev/null & disown
+for m in $(xrandr -q | grep " connected" | cut -d " " -f1); do
+	MONITOR="$m" polybar -q top --config=~/.config/polybar/config.ini > /dev/null & disown
+	MONITOR="$m" polybar -q bottom --config=~/.config/polybar/config.ini > /dev/null & disown
+done
